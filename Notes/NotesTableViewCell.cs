4888bd17572921da46934d8eb9733504c8dda328
David den Otter <dden@computronix.com>
2017-03-20 22:56:12 -0600

    Initial Commit Mostly playing with tableview to get it to autosize row when drawing moves outside of the current bounds.

---
+++ Notes/NotesTableViewCell.cs
@@ -0,0 +1,126 @@
+using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+
+namespace Notes
+{
+    public partial class NotesTableViewCell : UITableViewCell
+    {
+		CGPath path;
+
+		public WeakReference ViewController{get; set;}
+		public NSIndexPath indexPath;
+		bool touchesCaptured;
+
+        public NotesTableViewCell (IntPtr handle) : base (handle)
+        {
+			path = new CGPath();
+			touchesCaptured = false;
+
+			path.AddLines(new CGPoint[]{
+					new CGPoint(5,5),
+					new CGPoint(20,20),
+					new CGPoint(5,20)});
+			path.CloseSubpath();
+        }
+
+/*		public override UIView HitTest(CGPoint point, UIEvent uievent)
+		{
+			if (touchesCaptured)
+			{
+				Console.WriteLine("HitTest: TouchCaptured:true");
+				return this;
+			}
+			else
+			{
+				Console.WriteLine("HitTest: TouchCaptured:false");
+				return base.HitTest(point, uievent);
+			}
+		}
+*/	
+		public override void TouchesBegan(NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan(touches, evt);
+
+			Console.WriteLine("TouchesBegan: touchesCaptured:true");
+			touchesCaptured = true;
+			UITouch touch = touches.AnyObject as UITouch;
+			if (touch != null)
+			{
+				path.AddLineToPoint(touch.LocationInView(this));
+				SetNeedsDisplayInRect(CGRect.Inflate(path.BoundingBox, 5f, 5f));
+			}
+		}
+
+		/*
+		public override bool ShouldReceiveTouch(UIGestureRecognizer recognizer, UITouch touch)
+		{
+			if (touchesCaptured)
+			{
+				Console.WriteLine("ShouldReceiveTouch: TouchCaptured:true");
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("ShouldReceiveTouch: TouchCaptured:false");
+				return this.Frame.Contains(touch.LocationInView(this));
+			}
+		}
+		*/
+
+		public override void TouchesMoved(NSSet touches, UIEvent evt)
+		{
+			base.TouchesMoved(touches, evt);
+			Console.WriteLine("TouchesMoved: touchesCaptured:true");
+
+			UITouch touch = touches.AnyObject as UITouch;
+			if (touch != null)
+			{
+				path.AddLineToPoint(touch.LocationInView(this));
+				NotesTableViewController vc = (NotesTableViewController)ViewController.Target;
+
+				if (path.BoundingBox.Height + 20 > vc.RowHeights[indexPath.Row])
+				{
+					vc.RowHeights[indexPath.Row] = path.BoundingBox.Height + 20;
+					vc.UpdateViewConstraints();
+					vc.TableView.BeginUpdates();
+					vc.TableView.EndUpdates();
+					//vc.TableView.SetNeedsDisplay();
+					//InvokeOnMainThread(() => vc.TableView.ReloadRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Automatic));
+				}
+				SetNeedsDisplayInRect(CGRect.Inflate(path.BoundingBox, 5f, 5f));
+			}
+		}
+
+		public override void TouchesEnded(NSSet touches, UIEvent evt)
+		{
+			base.TouchesEnded(touches, evt);
+			Console.WriteLine("TouchesEnded: touchesCaptured:false");
+			touchesCaptured = false;
+			SetNeedsDisplayInRect(CGRect.Inflate(path.BoundingBox, 5f, 5f));
+		}
+
+		public override void TouchesCancelled(NSSet touches, UIEvent evt)
+		{
+			base.TouchesCancelled(touches, evt);
+			Console.WriteLine("TouchesCancelled: touchesCaptured:false");
+			touchesCaptured = false;
+		}
+
+		public override void Draw(CoreGraphics.CGRect rect)
+		{
+			base.Draw(rect);
+
+			using (CGContext g = UIGraphics.GetCurrentContext())
+			{
+				g.SetLineWidth(2);
+				UIColor.Blue.SetFill();
+				UIColor.Red.SetStroke();
+
+				g.AddPath(path);
+				g.DrawPath(CGPathDrawingMode.FillStroke);
+			}
+		}
+    }
+}
---
+++ Notes/NotesTableViewController.cs
@@ -0,0 +1,55 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Notes
+{
+	public partial class NotesTableViewController : UITableViewController
+	{
+
+
+		 static NSString NoteCellId = new NSString ("NotesTableViewCell");
+
+		public nfloat[] RowHeights;
+
+  
+		protected NotesTableViewController(IntPtr handle) : base(handle)
+		{
+			TableView.RegisterClassForCellReuse(typeof(NotesTableViewCell), NoteCellId);
+			RowHeights = new nfloat[]{ 50f, 50f, 50f, 50f, 50f, 50f };
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+		{
+			return 6;
+		}
+
+		public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+		{
+			return RowHeights[indexPath.Row];
+			
+			//return base.GetHeightForRow(tableView, indexPath);
+		}
+	
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+			var cell = (NotesTableViewCell)tableView.DequeueReusableCell(NoteCellId, indexPath);
+			cell.ViewController = new WeakReference(this);
+			cell.indexPath = indexPath;
+			return cell;
+		}
+
+	}
+}
